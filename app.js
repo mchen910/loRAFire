2514e611fb43b8e9d0457f1bb1a74b4f77327db0
Matthew Chen <matthewchen910@gmail.com>
2022-12-10 09:24:34 -0800

    added subdomain middleware

--- app.js
+++ app.js
@@ -1,41 +1,32 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var subdomain = require('./middleware/subdomain')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
+const port = 3000;
 
-var app = express();
+var subRouter = express.Router();
+var router = express.Router();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// API specific
+subRouter.get('/', (req, res) => {
+    res.send('API');
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+subRouter.get('/users', (req, res) => {
+    res.json([
+        { name: "Brian" }
+    ]);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+router.get('/', (req, res) => {
+    res.send('Hello world!');
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(subdomain('api', subRouter));
+app.use('/', router)
 
-module.exports = app;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
+
