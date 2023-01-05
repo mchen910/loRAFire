8d7a42454e836570ca082996d12245d363f8e5f1
Matthew Chen <matthewchen910@gmail.com>
2023-01-04 21:38:55 -0800

    completed existing node controllers

--- controllers/node.js
+++ controllers/node.js
@@ -1,24 +1,195 @@
+var { body, validationResult } = require('express-validator');
+
+var Node = require('../models/node');
+var Gateway = require('../models/gateway');
+
+
 // GET request for a list of nodes
-exports.GET_nodeList = (req, res) => {
-    res.send('not implemented yet, returns list of nodes');
+exports.node_index = async (req, res, next) => {
+    await Node.find(function(err, nodeList) {
+        if (err) {
+            next(err);
+            return;
+        }
+
+        res.status(200).json(nodeList);
+    })
 };
 
 // GET request for a node by its id
-exports.GET_nodeID = (req, res) => {
-    res.send('not implemented yet, returns a node from id');
+exports.node_new = async (req, res, next) => {
+    const node = await Node.findById(req.params.id);
+    const gateway = await Gateway.findById(node.gatewayID);
+
+    if (!node) {
+        var err = new Error("Node not found");
+        err.status = 400;
+        return next(err);
+    }
+    if (!gateway) {
+        var err = new Error("Invalid gateway ID");
+        err.status = 400;
+        return next(err);
+    }
+
+    res.status(200).json({
+        _id: node._id,
+        online: node.online,
+        location: node.location,
+        gateway: {
+            location: gateway.location,
+            online: gateway.online
+        }
+    });
 };
 
 // POST request for creating a new node entry
-exports.POST_node = (req, res) => {
-    res.send('not implemented yet, add a new node');
-}
+exports.node_create = [
+    body('name')
+        .optional()
+        .trim()
+        .isLength({ min: 5, max: 30 })
+        .escape()
+        .withMessage('Name must be between 5 and 30 characters.')
+        .isAlphanumeric()
+        .withMessage('Name has nonalphanumeric characters'),
+    body('location.latitude')
+        .isFloat({ min: -180.0, max: 180.0 })
+        .withMessage('Invalid latitude'),
+    body('location.longitude')
+        .isFloat({ min: -180.0, max: 180.0 })
+        .withMessage('Invalid longitude'),
+    body('online')
+        .isBoolean()
+        .withMessage('Not a boolean value'),
+    body('gatewayID')
+        .isMongoId()
+        .withMessage('Invalid gateway ID'),
+    body('hSensorID')
+        .optional()
+        .isMongoId()
+        .withMessage('Invalid gateway ID'),
+    body('sSensorID')
+        .optional()
+        .isMongoId()
+        .withMessage('Invalid gateway ID'),
+    
+    (req, res, next) => {
+        // Extract errors
+        const errors = validationResult(req);
 
-// DELETE request for deleting an existing node
-exports.DELETE_node = (req, res) => {
-    res.send('not implemented yet, delete a node');
-}
+        var node = new Node({
+            name: req.body.name,
+            location: req.body.location,
+            online: req.body.online,
+            gatewayID: req.body.gatewayID,
+            hSensorID: req.body.hSensorID,
+            sSensorID: req.body.sSensorID
+        });
+
+        if (!errors.isEmpty()) {
+            res.status(422).json({
+                errors: errors.array()
+            });
+            return;
+
+        } else {
+            node.save(function (err) {
+                if (err) {
+                    return next(err)
+                }
+            });
+        }
+    }
+]
 
 // UPDATE request for updating a node
-exports.PUT_node = (req, res) => {
-    res.send('not implemented yet, update a node')
+exports.node_update = [
+    body('name')
+        .optional()
+        .trim()
+        .isLength({ min: 5, max: 30 })
+        .escape()
+        .withMessage('Name must be between 5 and 30 characters.')
+        .isAlphanumeric()
+        .withMessage('Name has nonalphanumeric characters'),
+    body('location.latitude')
+        .isFloat({ min: -180.0, max: 180.0 })
+        .withMessage('Invalid latitude'),
+    body('location.longitude')
+        .isFloat({ min: -180.0, max: 180.0 })
+        .withMessage('Invalid longitude'),
+    body('online')
+        .isBoolean()
+        .withMessage('Not a boolean value'),
+    body('gatewayID')
+        .isMongoId()
+        .withMessage('Invalid gateway ID'),
+    body('hSensorID')
+        .optional()
+        .isMongoId()
+        .withMessage('Invalid gateway ID'),
+    body('sSensorID')
+        .optional()
+        .isMongoId()
+        .withMessage('Invalid gateway ID'),
+    
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        var node = new Node({
+            name: req.body.name,
+            location: req.body.location,
+            online: req.body.online,
+            gatewayID: req.body.gatewayID,
+            hSensorID: req.body.hSensorID,
+            sSensorID: req.body.sSensorID
+        });
+
+        if (!errors.isEmpty()) {
+            res.status(422).json({
+                errors: errors.array()
+            });
+            return;
+
+        } else {
+            Node.findByIdAndUpdate(
+                req.params.id,
+                node,
+                {},
+                function (err, _node) {
+                    if (err) {
+                        return next(err);
+                    }
+
+                    res.redirect(_node.url)
+                }
+            );
+        }
+    }
+]
+
+// DELETE request for deleting an existing node
+exports.node_destroy = async (req, res, next) => {
+    const node = Node.findById(req.params.id);
+    var gateway = Gateway.findById(node.gatewayID);
+
+    if (!node) {
+        var err = new Error("Node not found");
+        err.status = 400;
+        return next(err);
+    }
+    if (!gateway) {
+        var err = new Error("Invalid gateway ID");
+        err.status = 400;
+        return next(err);
+    }
+
+    // Delete the node from the gateway's list of nodes
+    let idx = gateway.nodes.indexOf(node);
+    gateway.nodes.splice(idx, 1);
+
+    // Finally delete the node
+    Node.findByIdAndRemove(req.params.id);
+    res.status(200).redirect('/api/v1/nodes')
 }
