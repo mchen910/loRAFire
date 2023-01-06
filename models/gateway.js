8a500c3dd279c4a74dd8ca9ec71980377ba0f36a
Matthew Chen <matthewchen910@gmail.com>
2023-01-06 11:31:55 -0800

    revised fields in gateway and node schemas

--- models/gateway.js
+++ models/gateway.js
@@ -4,17 +4,13 @@ var Schema = mongoose.Schema;
 var Types = mongoose.Types;
 
 const GatewaySchema = new Schema({
-    name: { 
-        type: String, 
-        minLength: 5, 
-        maxLength: 30, 
-    },
     location: {
         latitude: Number,
         longitude: Number,
     },
+    
     online: { type: Boolean, default: true },
-    nodes: [{type: Types.ObjectId, ref: 'Node'}]
+    nodes: [{type: Types.ObjectId}]
 },
     { versionKey: false }
 );
--- models/node.js
+++ models/node.js
@@ -4,12 +4,6 @@ var Schema = mongoose.Schema;
 var Types = mongoose.Types;
 
 const NodeSchema = new Schema({
-    name: {
-        type: String,
-        minLength: 5,
-        maxLength: 30,
-    },
-
     // There is no N/S, W/E, just positive and negative values
     location: {
         latitude: Number,
@@ -25,19 +19,6 @@ const NodeSchema = new Schema({
 
     { versionKey: false }
 )
-
-
-// // Virtual methods (getters for computed values)
-// NodeSchema.virtual('url').get(function() {
-//     return `/nodes/${this._id}`;
-// });
-
-// NodeSchema.virtual('location').get(function() {
-//     let lat = this.location.latitude, lon = this.location.longitude;
-//     lat += (lat > 0) ? "\u00b0N" : "\u00b0S";
-//     lon += (lon > 0) ? "\u00b0E" : "\u00b0W";
-//     return `${lat}, ${lon}`;
-// })
 
 
 module.exports = mongoose.model('Node', NodeSchema)
